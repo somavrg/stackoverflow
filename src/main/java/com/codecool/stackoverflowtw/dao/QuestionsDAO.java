fc71e06bf7da485b3f0454f567d0257d804a1323
Csaba <csabi627@gmail.com>
2023-08-29 13:43:53 +0200

    Create questionDAO interface

--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
@@ -1,5 +1,12 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.controller.dto.NewQuestionDTO;
+
 public interface QuestionsDAO {
     void sayHi();
+    void selectAll();
+    void selectById(int id);
+    void deleteQuestionById(int id);
+
+    void addNewQuestion(NewQuestionDTO question);
 }
--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
@@ -1,8 +1,30 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.controller.dto.NewQuestionDTO;
+
 public class QuestionsDaoJdbc implements QuestionsDAO {
     @Override
     public void sayHi() {
         System.out.println("Hi DAO!");
+    }
+
+    @Override
+    public void selectAll() {
+
+    }
+
+    @Override
+    public void selectById(int id) {
+
+    }
+
+    @Override
+    public void deleteQuestionById(int id) {
+
+    }
+
+    @Override
+    public void addNewQuestion(NewQuestionDTO question) {
+
     }
 }
