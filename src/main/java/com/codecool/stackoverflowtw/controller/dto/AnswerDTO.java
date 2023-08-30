461b1d6694a61df2bd32598c3f9dda5d5fee1599
Kapus Tamás <kapustamas@gmail.com>
2023-08-30 18:14:58 +0200

    feat: implement AnswerService

--- src/main/java/com/codecool/stackoverflowtw/controller/dto/AnswerDTO.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/dto/AnswerDTO.java
@@ -2,4 +2,4 @@ package com.codecool.stackoverflowtw.controller.dto;
 
 import java.time.LocalDateTime;
 
-public record AnswerDTO(int id, String text, LocalDateTime date, int questionId) {}
+public record AnswerDTO(int id, String text, LocalDateTime date, int questionId,int score) {}
