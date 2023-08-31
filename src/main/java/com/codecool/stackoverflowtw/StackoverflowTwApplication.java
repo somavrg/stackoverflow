3c57eec7bdddca0be738c9996799b150814d7e50
oliver-nagy <olivernagy490@gmail.com>
2023-08-31 10:39:46 +0200

    refactor: refactor JdbcConnection classname

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -4,12 +4,10 @@ import com.codecool.stackoverflowtw.dao.AnswerDAO;
 import com.codecool.stackoverflowtw.dao.AnswerDaoJdbc;
 import com.codecool.stackoverflowtw.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.QuestionsDaoJdbc;
-import com.codecool.stackoverflowtw.database.jdbcConnection;
+import com.codecool.stackoverflowtw.database.JdbcConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.sql.Connection;
 
 @SpringBootApplication
 public class StackoverflowTwApplication {
@@ -23,26 +21,26 @@ public class StackoverflowTwApplication {
         SpringApplication.run(StackoverflowTwApplication.class, args);
     }
 
-    private static jdbcConnection getConnection() {
+    private static JdbcConnection getConnection() {
         String dbName = System.getenv(DATABASE_NAME);
         String userName = System.getenv(USER_NAME);
         String password = System.getenv(PASSWORD);
         String URL = "jdbc:postgresql://localhost:5432/" + dbName;
 
-        jdbcConnection connection = new jdbcConnection(dbName, userName, password, URL);
+        JdbcConnection connection = new JdbcConnection(dbName, userName, password, URL);
         return connection;
     }
 
     @Bean
     public QuestionsDAO questionsDAO() {
-        jdbcConnection databaseConnection = getConnection();
+        JdbcConnection databaseConnection = getConnection();
 
         return new QuestionsDaoJdbc(databaseConnection);
     }
 
     @Bean
     public AnswerDAO answerDAO() {
-        jdbcConnection databaseConnection = getConnection();
+        JdbcConnection databaseConnection = getConnection();
         return new AnswerDaoJdbc(databaseConnection);
     }
 }
